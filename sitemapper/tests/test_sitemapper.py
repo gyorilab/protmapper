4381a6c8957de06ab036390ca04f7f55d56b9403
John Bachman <bachmanjohn@gmail.com>
2018-12-15 18:00:01 -0500

    Rewrite tests to use MappedSite equality

--- sitemapper/tests/test_sitemapper.py
+++ sitemapper/tests/test_sitemapper.py
@@ -36,96 +36,84 @@ def test_validate_site():
     assert valid_site[1] == '185'
 
 
+def test_mapped_site_equals():
+    ms1 = MappedSite(up_id='P28482', valid=False, orig_res='T',
+                     orig_pos='183', mapped_res='T', mapped_pos='185',
+                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
+    ms2 = MappedSite(up_id='P28482', valid=False, orig_res='T',
+                     orig_pos='183', mapped_res='T', mapped_pos='185',
+                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
+    assert ms1 == ms2
+    ms2.gene_name = 'FOO'
+    assert ms1 != ms2
+
+
+def test_mapped_site_hash():
+    ms1 = MappedSite(up_id='P28482', valid=False, orig_res='T',
+                     orig_pos='183', mapped_res='T', mapped_pos='185',
+                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
+    ms2 = MappedSite(up_id='P28482', valid=False, orig_res='T',
+                     orig_pos='183', mapped_res='T', mapped_pos='185',
+                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
+    assert hash(ms1) == hash(ms2)
+    ms2.gene_name = 'FOO'
+    assert hash(ms1) != hash(ms2)
+
+
 def test_check_agent_mod_up_id():
     sm = SiteMapper()
     ms = sm.map_to_human_ref('P28482', 'uniprot', 'T', '185')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'P28482'
-    assert ms.gene_name == 'MAPK1'
-    assert ms.valid is True
-    assert ms.orig_res == 'T'
-    assert ms.orig_pos == '185'
-    assert ms.mapped_res == 'T'
-    assert ms.mapped_pos == '185'
-    assert ms.description == 'VALID'
+    assert ms == MappedSite(up_id='P28482', valid=True, orig_res='T',
+                            orig_pos='185', mapped_res='T', mapped_pos='185',
+                            description='VALID', gene_name='MAPK1')
 
     ms = sm.map_to_human_ref('P28482', 'uniprot', 'T', '183')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'P28482'
-    assert ms.gene_name == 'MAPK1'
-    assert ms.valid is False
-    assert ms.orig_res == 'T'
-    assert ms.orig_pos == '183'
-    assert ms.mapped_res == 'T'
-    assert ms.mapped_pos == '185'
-    assert ms.description == 'INFERRED_MOUSE_SITE'
+    assert ms == MappedSite(up_id='P28482', valid=False, orig_res='T',
+                            orig_pos='183', mapped_res='T', mapped_pos='185',
+                            description='INFERRED_MOUSE_SITE',
+                            gene_name='MAPK1')
 
 
 def test_check_agent_mod_hgnc():
     sm = SiteMapper()
     ms = sm.map_to_human_ref('MAPK1', 'hgnc', 'T', '185')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'P28482'
-    assert ms.gene_name == 'MAPK1'
-    assert ms.valid is True
-    assert ms.orig_res == 'T'
-    assert ms.orig_pos == '185'
-    assert ms.mapped_res == 'T'
-    assert ms.mapped_pos == '185'
-    assert ms.description == 'VALID'
+    assert ms == MappedSite(up_id='P28482', valid=True, orig_res='T',
+                            orig_pos='185', mapped_res='T', mapped_pos='185',
+                            description='VALID', gene_name='MAPK1')
 
     ms = sm.map_to_human_ref('MAPK1', 'hgnc', 'T', '183')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'P28482'
-    assert ms.gene_name == 'MAPK1'
-    assert ms.valid is False
-    assert ms.orig_res == 'T'
-    assert ms.orig_pos == '183'
-    assert ms.mapped_res == 'T'
-    assert ms.mapped_pos == '185'
-    assert ms.description == 'INFERRED_MOUSE_SITE'
+    assert ms == MappedSite(up_id='P28482', valid=False, orig_res='T',
+                            orig_pos='183', mapped_res='T', mapped_pos='185',
+                            description='INFERRED_MOUSE_SITE',
+                            gene_name='MAPK1')
 
 
 def test_map_mouse_site():
     sm = SiteMapper()
     ms = sm.map_to_human_ref('THEMIS2', 'hgnc', 'Y', '660')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'Q5TEJ8'
-    assert ms.gene_name == 'THEMIS2'
-    assert ms.valid is False
-    assert ms.orig_res == 'Y'
-    assert ms.orig_pos == '660'
-    assert ms.mapped_res == 'Y'
-    assert ms.mapped_pos == '632'
-    assert ms.description == 'INFERRED_MOUSE_SITE'
+    assert ms == MappedSite(up_id='Q5TEJ8', valid=False, orig_res='Y',
+                            orig_pos='660', mapped_res='Y', mapped_pos='632',
+                            description='INFERRED_MOUSE_SITE',
+                            gene_name='THEMIS2')
 
 
 def test_map_rat_site():
     sm = SiteMapper()
     ms = sm.map_to_human_ref('NPHS1', 'hgnc', 'Y', '1204')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'O60500'
-    assert ms.gene_name == 'NPHS1'
-    assert ms.valid is False
-    assert ms.orig_res == 'Y'
-    assert ms.orig_pos == '1204'
-    assert ms.mapped_res == 'Y'
-    assert ms.mapped_pos == '1193'
-    assert ms.description == 'INFERRED_RAT_SITE'
+    assert ms == MappedSite(up_id='O60500', valid=False, orig_res='Y',
+                            orig_pos='1204', mapped_res='Y', mapped_pos='1193',
+                            description='INFERRED_RAT_SITE',
+                            gene_name='NPHS1')
 
 
 def test_map_methionine_cleavage():
     sm = SiteMapper()
     ms = sm.map_to_human_ref('DAXX', 'hgnc', 'S', '667')
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'Q9UER7'
-    assert ms.gene_name == 'DAXX'
-    assert ms.valid is False
-    assert ms.orig_res == 'S'
-    assert ms.orig_pos == '667'
-    assert ms.mapped_res == 'S'
-    assert ms.mapped_pos == '668'
-    assert ms.description == 'INFERRED_METHIONINE_CLEAVAGE'
+    assert ms == MappedSite(up_id='Q9UER7', valid=False, orig_res='S',
+                            orig_pos='667', mapped_res='S', mapped_pos='668',
+                            description='INFERRED_METHIONINE_CLEAVAGE',
+                            gene_name='DAXX')
+
 
 def test_repr_str():
     sm = SiteMapper()
@@ -134,6 +122,7 @@ def test_repr_str():
                        "orig_res='T', orig_pos='183', mapped_res='T', "
                        "mapped_pos='185', description='INFERRED_MOUSE_SITE', "
                        "gene_name='MAPK1')")
+
 
 def test_read_cache():
     sm = SiteMapper(use_cache=True, cache_path='test_cache_read.pkl')
@@ -148,6 +137,7 @@ def test_read_cache():
     assert ms.mapped_pos == 'TEST7'
     assert ms.description == 'TEST8'
 
+
 def test_write_cache():
     cache_path = 'test_cache_write.pkl'
     assert not os.path.isfile(cache_path)
@@ -160,40 +150,10 @@ def test_write_cache():
     assert isinstance(cache_dict, dict)
     assert len(cache_dict) == 1
     ms = cache_dict[('P28482', 'T', '183')]
-    assert isinstance(ms, MappedSite)
-    assert ms.up_id == 'P28482'
-    assert ms.gene_name == 'MAPK1'
-    assert ms.valid is False
-    assert ms.orig_res == 'T'
-    assert ms.orig_pos == '183'
-    assert ms.mapped_res == 'T'
-    assert ms.mapped_pos == '185'
-    assert ms.description == 'INFERRED_MOUSE_SITE'
+    assert ms == MappedSite(up_id='P28482', valid=False, orig_res='T',
+                            orig_pos='183', mapped_res='T', mapped_pos='185',
+                            description='INFERRED_MOUSE_SITE',
+                            gene_name='MAPK1')
     os.unlink(cache_path)
 
 
-def test_mapped_site_equals():
-    ms1 = MappedSite(up_id='P28482', valid=False, orig_res='T',
-                     orig_pos='183', mapped_res='T', mapped_pos='185',
-                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
-    ms2 = MappedSite(up_id='P28482', valid=False, orig_res='T',
-                     orig_pos='183', mapped_res='T', mapped_pos='185',
-                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
-    assert ms1 == ms2
-    ms2.gene_name = 'FOO'
-    assert ms1 != ms2
-
-def test_mapped_site_hash():
-    ms1 = MappedSite(up_id='P28482', valid=False, orig_res='T',
-                     orig_pos='183', mapped_res='T', mapped_pos='185',
-                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
-    ms2 = MappedSite(up_id='P28482', valid=False, orig_res='T',
-                     orig_pos='183', mapped_res='T', mapped_pos='185',
-                     description='INFERRED_MOUSE_SITE', gene_name='MAPK1')
-    assert hash(ms1) == hash(ms2)
-    ms2.gene_name = 'FOO'
-    assert hash(ms1) != hash(ms2)
-
-
-if __name__ == '__main__':
-    test_mapped_site_hash()
