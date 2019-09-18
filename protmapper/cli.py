f0b65f5fcacefa4c663c11ad1e5655a7fb09dc3d
Ben Gyori <ben.gyori@gmail.com>
2019-09-18 07:42:47 -0400

    Dump output and make arguments positional

--- protmapper/cli.py
+++ protmapper/cli.py
@@ -13,18 +13,25 @@ def process_input(fname):
     return sites
 
 
+def dump_output(fname, mapped_sites):
+    rows = [ms.to_list() for ms in mapped_sites]
+    with open(fname, 'w') as fh:
+        writer = csv.writer(fh)
+        writer.writerows(rows)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Run Protmapper on a list of proteins with residues and '
                     'sites provided in a text file.')
-    parser.add_argument('--input',
+    parser.add_argument('input',
         help=('Path to an input file. The input file is a text file in '
               'which each row consists of four comma separated values, '
               'with the first element being a protein ID, the second, '
               'the namespace in which that ID is valid (e.g., UP, HGNC),'
               'third, an amino acid represented as a single capital letter, '
               'and fourth, a site position on the protein.'), required=True)
-    parser.add_argument('--output',
+    parser.add_argument('output',
         help=('Path to the output file to be generated. Each line of the '
               'output file corresponds to a line in the input file. Each line'
               'represents a mapped site produced by Protmapper.'),
@@ -34,3 +41,4 @@ if __name__ == '__main__':
     pm = ProtMapper()
     sites = process_input(args.input)
     mapped_sites = pm.map_sitelist_to_human_ref(sites)
+    dump_output(args.output, mapped_sites)
